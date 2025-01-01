80663e9acb1736a8253ead6d75716bb9fbdc21fb
Harry <harry.d.yin.gpc@gmail.com>
2024-12-31 18:09:30 -0800

    moved to torch.float8_e4m3fn

--- src/summarizer.py
+++ src/summarizer.py
@@ -19,7 +19,7 @@ def load_model():
         "summarization",
         model="sshleifer/distilbart-cnn-12-6",
         device=device,
-        torch_dtype=torch.int8
+        torch_dtype=torch.float8_e4m3fn
     )
 summarizer = load_model()
 logger.info("Pipeline established...")
