72ec47c56471ca08aee2eaeeea32ac8010aa019f
Harry <harry.d.yin.gpc@gmail.com>
2025-01-03 18:25:04 -0800

    added borders

--- src/pages/article_page.py
+++ src/pages/article_page.py
@@ -66,8 +66,13 @@ with col1:
 with col2:
     st.write("Estimated Time Taken:")
     st.write(f"```{st.session_state['estimated_time']}``` seconds")
+
+st.write("---")
+
 light_checkbox = st.checkbox("Run Light Version", True, key="Light Version")
 stoggle("ⓘ What is Light Version?", "Light Version means that instead of summarizing the topics, the tool will only provide the representative sentences in its objective form. This is much faster and less resource-intensive, however, it may not provide as much information.")
+
+st.write("---")
 
 def disable_analyze():
     st.session_state["article_disabled"] = True
